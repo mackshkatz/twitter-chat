8657d044728c52f2d7f827dac0b6a4e9d196d4d1
Max Katz <maxwelljkatz@gmail.com>
2012-04-30 13:53:00 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwitterChat::Application.config.secret_token = '837051c75a095132faeada57a2842d3cec3bcbaea7a905e34d26db5b5e3261d55335903b22b20286cd04798cf7fad60af4f77947653186c7a3082ed05a809709'
