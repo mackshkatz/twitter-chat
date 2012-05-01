a3a74f89b8408e3f7545f8dc5c3314b352723cab
Max Katz <maxwelljkatz@gmail.com>
2012-05-01 13:22:56 -0600

    Set up javascript application namespace

--- app/assets/javascripts/twitter_chat.js
+++ app/assets/javascripts/twitter_chat.js
@@ -1,10 +1,44 @@
-$(function() {
-	$('.tweet-list').append(JST['pages/index']({
-		time: "",
-		image_src: "",
-		screen_name: "",
-		real_name: "",
-		tweet_body: "",
-		tweet_url: "",
-	}));
+window.twitter_chat = {
+
+	init: function() {
+		this.bindEvents();
+	},
+
+	bindEvents: function() {
+		$('.search-button').click(function() {
+			var search_query = $('.search-query').val();
+			twitter_chat.getTweets(search_query);
+		});
+	},
+
+	getTweets: function(search_query) {
+		$.ajax({
+			url: search_query,
+			success: function(response) {
+				console.log(response)
+				// retrieve tweet and pull out necessary date for template
+				// pass it through template and append to DOM
+			},
+			dataType: "jsonp",
+			complete: function(response) {
+				console.log("completed", response)
+				// if tweet is part of convo, request another tweet part of that convo
+			}
+		})
+	}
+
+	// context: {
+	// 	time: "1",
+	// 	avatar: "",
+	// 	screen_name: "1",
+	// 	real_name: "1",
+	// 	tweet_body: "1",
+	// 	tweet_url: "1",
+	// }
+
+	// $('.tweet-list').append(JST['pages/index']("test"));
+}
+
+$(document).ready(function() {
+	twitter_chat.init();
 });
